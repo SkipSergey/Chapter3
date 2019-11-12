a5e95969bf677227034e9375a3d395570b9876ea
Serg Chisel <serg_kisel@bk.ru>
2019-11-12 12:19:25 +0200

    Добавьте файлы проекта.

---
+++ Chapter3/Cargo.cs
@@ -0,0 +1,14 @@
+﻿namespace Chapter3
+{
+    public struct Cargo
+    {
+        public object cargo;
+        public double weight;
+
+        public Cargo(object cargo, double weight)
+        {
+            this.cargo = cargo;
+            this.weight = weight;
+        }
+    }
+}
